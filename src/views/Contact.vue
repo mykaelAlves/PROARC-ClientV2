892e6edb6662da58a9d416c6aead90a13064d2de
makai <mykaelalvesnogueira1@gmail.com>
2025-03-20 20:07:25 -0300

    trying to understand routing

---
+++ src/views/Contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="container">
+    <h1>Contact</h1>
+    <p>This is a boilerplate Vue 3 component.</p>
+  </main>
+</template>
+
+<style scoped>
+.container {
+  margin: 0;
+  padding: 20px;
+  text-align: center;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+
+h1 {
+  font-size: 2em;
+  margin-bottom: 0.5em;
+}
+
+p {
+  font-size: 1em;
+  color: #666;
+}
+</style>
