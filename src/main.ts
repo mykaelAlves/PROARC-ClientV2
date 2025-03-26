6a7cf5c71e68678ddf80a836efb5f7b385036213
makai <mykaelalvesnogueira1@gmail.com>
2025-03-26 13:02:16 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import { invoke } from "@tauri-apps/api/core";
 
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
+let greetInputEl: HTMLInputElement | null;
+let greetMsgEl: HTMLElement | null;
+
+async function greet() {
+  if (greetMsgEl && greetInputEl) {
+    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    greetMsgEl.textContent = await invoke("greet", {
+      name: greetInputEl.value,
+    });
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    greet();
+  });
+});
